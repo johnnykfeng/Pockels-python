4a6a2f1ed04cd590e56e84f2507b149b9fd595fe
John Feng <johnfengphd@gmail.com>
2024-02-01 09:37:29 -0800

    updated image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -1,9 +1,7 @@
-import sys
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from DEPRECATED.load_files import txt2matrix
-from skimage import filters, feature, io
+from data_parser import DataParser
 from scipy.ndimage import uniform_filter1d
 from scipy.interpolate import interp1d
 from loguru_pack import logger, loguru_config
@@ -20,8 +18,9 @@ class ImageProcessor:
         self.edge_1_fit = None
         self.edge_2_fit = None
         self.padding = 10
-        self.cropped_image = None
+        self.image_with_edges = None
         self.image_corrected = None # output of correct_distortion
+        self.image_corrected_cropped = None 
         self.cleaned_image = None # output of remove_bright_speckes
         
     @staticmethod
@@ -112,8 +111,10 @@ class ImageProcessor:
 
         # array of new pixel positions in the y-axis
         y_corrected = np.arange(round(np.mean(self.edge_1_fit) - self.padding), round(np.mean(self.edge_2_fit)) + self.padding)
+        # crop self.image_corrected to the region of interest
+        self.image_corrected_cropped = self.image_corrected[y_corrected[0]: y_corrected[-1], :]
 
-        logger.debug(f"{len(y_corrected) = }")
+        logger.debug(f"{self.image_corrected.shape = }")
 
         for col_index in range(width):
             # Calculate the vertical bounds for the current column, adjusted by self.padding
@@ -121,23 +122,22 @@ class ImageProcessor:
             edge_2_crop = int(self.edge_2_fit[col_index] + self.padding)
             # Extract a vertical slice (column) of the image within the calculated bounds
             column_slice = raw_image[edge_1_crop:edge_2_crop, col_index]
-            # Create an array representing the original vertical positions in the slice
+ 
             original_positions = np.arange(len(column_slice))
-            # Create a linearly spaced array for the new vertical positions
+            logger.debug(f"{len(original_positions) = }")
+
             new_positions = np.arange(1, len(y_corrected))
-            # Perform linear interpolation from the original positions to the new positions
-            # 'interp1d' creates a linear interpolation function
-            # 'fill_value='extrapolate'' allows the function to handle values outside the domain
+            logger.debug(f"{len(new_positions) = }")
+ 
             interpolated_values = interp1d(original_positions, column_slice, 
                                         kind='linear', fill_value='extrapolate')(new_positions)
+            logger.debug(f"{len(interpolated_values) = }")
+            self.image_corrected[y_corrected[0]:y_corrected[-1], col_index] = interpolated_values
+            self.image_corrected_cropped[:, col_index] = interpolated_values
 
-            # Assign the interpolated values to the corresponding column in the corrected image
-            # 'y_corrected' defines the vertical range in the corrected image where the interpolated values are placed
-            self.image_corrected[y_corrected[0]: y_corrected[-1], col_index] = interpolated_values
+        return self.image_corrected, self.image_corrected_cropped
 
-        return self.image_corrected
-
-    def plot_edge_fit(self):
+    def show_image_with_edges(self, plot_image = False):
         # Ensure edge_1_fit and edge_2_fit are defined and accessible
         plt.figure("Image with Edges")
 
@@ -147,26 +147,35 @@ class ImageProcessor:
         # Generate x-coordinates for the edges
         rows = np.arange(self.image_calib.shape[1])
 
+        # create a new image with the edges and padding marked, by setting the values of the edges to 0 and padding to max
+        self.image_with_edges = np.copy(self.image_calib)
+        self.image_with_edges[self.edge_1_fit, rows] = 0
+        self.image_with_edges[self.edge_2_fit, rows] = 0
+        self.image_with_edges[self.edge_1_fit - self.padding, rows] = np.max(self.image_with_edges)
+        self.image_with_edges[self.edge_2_fit + self.padding, rows] = np.max(self.image_with_edges)
+
         # Plot edge_1_fit and edge_2_fit
-        plt.plot(rows, self.edge_1_fit, color='black', label='Edge 1 fit', alpha=1)
-        plt.plot(rows, self.edge_2_fit, color='green', label='Edge 2 fit', alpha=1)
+        # plt.plot(rows, self.edge_1_fit, color='black', label='Edge 1 fit', alpha=1)
+        # plt.plot(rows, self.edge_2_fit, color='green', label='Edge 2 fit', alpha=1)
 
         # Plot edges with padding
-        plt.plot(rows, self.edge_1_fit - self.padding, color='magenta', label='padding')
-        plt.plot(rows, self.edge_2_fit + self.padding, color='magenta')
+        # plt.plot(rows, self.edge_1_fit - self.padding, color='magenta', label='padding')
+        # plt.plot(rows, self.edge_2_fit + self.padding, color='magenta')
 
-        plt.colorbar()
-        plt.legend()
-        plt.show()
+        if plot_image:
+            plt.figure("Image with Edges")
+            plt.imshow(self.image_with_edges, cmap='jet')
+            plt.colorbar()
+            plt.show()
 
-    def remove_bright_speckles(self, threshold_scale=1.2):
+    def remove_bright_speckles(self, threshold_scale=1.5):
         """
         args:
             threshold_scale: float, default=1.2
                 Lower values will remove more speckles, but may also remove valid features.
                 Best values are between 1.2 and 1.6, determined by trial and error.
         """
-        self.cleaned_image = np.copy(self.image_corrected)
+        self.cleaned_image = np.copy(self.image_corrected_cropped)
         for y in range(2, self.cleaned_image.shape[0] - 2):
             for x in range(2, self.cleaned_image.shape[1] - 2):
                 neighbors = self.cleaned_image[y-2:y+3, x-2:x+3].ravel()
@@ -183,27 +192,32 @@ class ImageProcessor:
 def main():
     data_folder = r"test_data\D325150"
     bias_txt = os.path.join(data_folder, '700V Crossed 25mA.txt')
-    bias_image = txt2matrix(bias_txt)
+    DP = DataParser()
+    bias_image = DP.txt2array(bias_txt)
     parallel_0V_txt = os.path.join(data_folder, '0V Parallel.txt')
-    parallel_image = txt2matrix(parallel_0V_txt)
+    parallel_image = DP.txt2array(parallel_0V_txt)
 
     processor = ImageProcessor(parallel_image)
     processor.find_sensor_edges()
     img_corrected = processor.correct_distortion(raw_image=bias_image)
     print(processor.__dict__.keys())
-    # processor.plot_edge_fit()
+    processor.show_image_with_edges(plot_image=True)
 
+    # crop the image to the region of interest
     xmin, xmax = 450, 690
     ymin, ymax = 115, 225
+    img_corrected, img_corrected_cropped = processor.correct_distortion(raw_image=bias_image)
     plt.figure("corrected and cropped bias image")
     # set vmin and vmax to the same values to compare images
-    plt.imshow(img_corrected[ymin:ymax, xmin:xmax], cmap='jet', vmin=0, vmax=700)
+    # plt.imshow(img_corrected[ymin:ymax, xmin:xmax], cmap='jet', vmin=0, vmax=700)
+    plt.imshow(img_corrected_cropped, cmap='jet', vmin=0, vmax=700)
     plt.colorbar()
 
     for threshold_scale in [1.2, 1.4, 1.6]:
         cleaned = processor.remove_bright_speckles(threshold_scale=threshold_scale)
         plt.figure(f"cleaned image threshold_scale={threshold_scale}")
-        plt.imshow(cleaned[ymin:ymax, xmin:xmax], cmap='jet', vmin=0, vmax=700)
+        # plt.imshow(cleaned[ymin:ymax, xmin:xmax], cmap='jet', vmin=0, vmax=700)
+        plt.imshow(cleaned, cmap='jet', vmin=0, vmax=700)
         plt.colorbar()
 
     plt.show()
